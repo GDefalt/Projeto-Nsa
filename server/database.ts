9e88cc6f4061a4688cea3e9c9df26e6cf27bdff4
GDefalt <bluegamer2627@hotmail.com>
2024-03-22 19:14:02 -0300

    2

--- server/database.ts
+++ server/database.ts
@@ -1,6 +1,12 @@
 import sqlite3 from 'sqlite3';
+import { resolve, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
 
-const db = new sqlite3.Database('./mydatabase.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = resolve(__filename, '..')
+const dbPath = join(__dirname, 'db', 'app.db')
+
+const db = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
   if (err) {
     console.error(err.message);
   } else {
